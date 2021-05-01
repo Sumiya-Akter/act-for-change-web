bbdb81b1f2889ced1a9053d8affad0931ab0d89a
Glitch (act-for-change) <none>
2021-05-01 22:41:07 +0000

    💍🏵 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,9 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints a message in the browser's dev tools console
+console.log("Hello 🌎");
+
+function onSubmit(){
+  alert("Thank you for your interest. Your email has been recorded. We will send climate change newsletters to the email listed.")
+}
